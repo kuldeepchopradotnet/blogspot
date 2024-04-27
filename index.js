795add416d5c7f8efe1d71c4f7aa142998d0419d
kuldeep <kuldeepchopradotnet@gmail.com>
2024-04-27 15:00:59 +0530

    hideSideBar

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function hello() {
     alert("hello")
 }
+
+
+function hideSideBar() {
+    let sideBar = document.getElementById("side-bar");
+    if(sideBar.style.display === 'none'){
+        sideBar.style.display === ''
+    }
+    else {
+        sideBar.style.display === 'none'
+    }
+}
